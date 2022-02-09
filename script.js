49a73236d681dfe9af843eceec0ecab5c6a42b56
adri <amilis@student.s19.be>
2022-02-09 11:47:30 +0100

    finished product

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ function setGrid(elements) {
     let new_elements = (elements ** 2) - (number_of_cells ** 2);
 
     if (new_elements > 0) {
-        console.log(new_elements);
         for (let i = 0; i < new_elements; ++i) {
             const new_div = document.createElement('div');
             new_div.classList.add('cell');
             container.appendChild(new_div);
             new_div.addEventListener('mouseover', () => {
-                new_div.style.cssText = 'background-color: black';
+                new_div.style.cssText = `background-color: ${drawColor}`;
             });
         }
     }
@@ -29,7 +28,6 @@ function setGrid(elements) {
     }
 
     number_of_cells = elements;
-    console.log(number_of_cells);
 }
 
 
@@ -38,7 +36,13 @@ function resetGrid() {
     all_cells.forEach((item) => {item.style.cssText = 'background-color: white'});
 }
 
+function makeNewGrid(slider) {
+    resetGrid();
+    setGrid(parseInt(slider.value));
+}
+
 let number_of_cells = 0;
+let drawColor = 'black';
 
 let reset_button = document.querySelector('.reset');
 reset_button.addEventListener('click', resetGrid);
@@ -56,8 +60,23 @@ slider.oninput = function() {
 // When the user stops on a number, generates the new grid
 slider.onchange = function() {
     resetGrid();
-    console.log(parseInt(slider.value));
     setGrid(parseInt(slider.value));
 }
 
+
+let colors = document.querySelectorAll('.color');
+for (let i = 0; i < colors.length; ++i) {
+    colors[i].addEventListener('click', () => {
+        let style = window.getComputedStyle(colors[i]);
+        drawColor = style.getPropertyValue('background-color');
+        setGrid(parseInt(slider.value));
+        let yourColor = document.getElementById('your-color');
+        yourColor.style.cssText = `background-color: ${drawColor}`;
+    });
+}
+
+let yourColor = document.getElementById('your-color');
+yourColor.style.cssText = `background-color: ${drawColor}`;
+
 setGrid(16);
+
